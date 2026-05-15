4c6fd2a11d0362403d0e2f7b76ee097a84b4c30f
Priyesh Shetty <shettypriyesh761@gmail.com>
2026-05-15 23:05:20 +0530

    Add: combination-sum-iv - submission-0

---
+++ Data Structures & Algorithms/combination-sum-iv/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        
+        int[] dp = new int[target+1];
+        dp[0] = 1;
+        for(int i=0;i<=target;i++) {
+            if(dp[i]==0) continue;
+            for(int j: nums) {
+                if(i+j>target) continue;
+                dp[i+j] += dp[i];
+            }
+        }
+        return dp[target];
+    }
+}
