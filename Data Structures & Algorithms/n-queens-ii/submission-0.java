5697d1987ac25d1523d613a726aecb3dfb3e57a3
Priyesh Shetty <shettypriyesh761@gmail.com>
2026-07-11 23:31:26 +0530

    Add: n-queens-ii - submission-0

---
+++ Data Structures & Algorithms/n-queens-ii/submission-0.java
@@ -0,0 +1,40 @@
+class Solution {
+    int[][] dir =
+        new int[][] {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {1, -1}, {-1, -1}, {-1, 1}};
+
+    boolean check(Set<List<Integer>> st, int x, int y, int n, int dx, int dy) {
+        int i = dx, j = dy;
+        while (x+i > 0 && y+j > 0 && x + i <= n && y + j <= n) {
+            if (st.contains(List.of(x + i, y + j)))
+                return false;
+            i += dx;
+            j += dy;
+        }
+        return true;
+    }
+
+    public int calc(Set<List<Integer>> st, int row, int n) {
+        if (row == 0) {
+            return 1;
+        }
+        int res = 0;
+        for (int col = 1; col <= n; col++) {
+            boolean safe = true;
+            for (int[] dr : dir) {
+                safe = safe && check(st, row, col, n, dr[0], dr[1]);
+            }
+            if (safe) {
+                List<Integer> entry = List.of(row, col);
+                st.add(entry);
+                res += calc(st, row - 1, n);
+                st.remove(entry);
+            }
+        }
+        return res;
+    }
+
+    public int totalNQueens(int n) {
+        Set<List<Integer>> st = new HashSet<>();
+        return calc(st, n, n);
+    }
+}
